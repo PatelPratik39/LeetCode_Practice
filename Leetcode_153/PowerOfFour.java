2c5086551ddc9adc6a01898ee0e4d630ae337ae8
PatelPratik39 <ptl.pratik717@gmail.com>
2024-08-13 19:33:10 -0500

    LeetCode problem 342. Power of Four

---
+++ Leetcode_153/PowerOfFour.java
@@ -0,0 +1,22 @@
+package Leetcode_153;
+
+public class PowerOfFour {
+    private static boolean isPowerOfFour ( int n ) {
+//        int i = 1;
+        if( n <= 0){
+            return false;
+        }
+        while( n % 4 == 0){
+            n = n /4;
+        }
+        return n == 1;
+    }
+
+
+    public static void main ( String[] args ) {
+        int n =16;
+        System.out.println(isPowerOfFour(n));
+    }
+
+
+}
