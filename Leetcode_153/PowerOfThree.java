c1b2aa80b0e72c496d92a0a5afeb594222d42431
PatelPratik39 <ptl.pratik717@gmail.com>
2024-08-13 19:30:17 -0500

    LeetCode problem 326. Power of Three

---
+++ Leetcode_153/PowerOfThree.java
@@ -0,0 +1,18 @@
+package Leetcode_153;
+
+public class PowerOfThree {
+    private static boolean isPowerOfThree ( int n ) {
+
+        int i = 1;
+         while(i < n){
+             i = i * 3;
+         }
+         return i == n;
+    }
+    public static void main ( String[] args ) {
+        int n = 27;
+        System.out.println( isPowerOfThree(n));
+    }
+
+
+}
